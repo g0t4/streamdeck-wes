4de5f6adf17492eaf18d3fdefbb02c006a06097f
Wes Higbee <wes.mcclure@gmail.com>
2025-11-11 12:48:05 -0600

    rename

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,5 +1,5 @@
 import streamDeck, { LogLevel } from "@elgato/streamdeck";
-import { TriggerMacro } from "./actions/trigger-macro";
+import { TriggerMacro } from "./km/macro";
 
 streamDeck.logger.setLevel(LogLevel.TRACE); // min threshold to capture
 // streamDeck.logger.setLevel(LogLevel.DEBUG); // defaults otherwise: DEBUG in dev, INFO in prod
