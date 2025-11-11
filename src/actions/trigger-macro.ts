aac300e3fafe1d1e2d9d1d796fa9a55a02ec227a
Wes Higbee <wes.mcclure@gmail.com>
2025-11-10 20:25:18 -0600

    trigger macro

--- src/actions/trigger-macro.ts
+++ src/actions/trigger-macro.ts
@@ -13,45 +13,20 @@ import {
 } from "@elgato/streamdeck";
 import { KeyboardMaestroHelper } from './km';
 
-
 @action({ UUID: "com.wes.kmtrigger.macro" })
 export class TriggerMacro extends SingletonAction<CounterSettings> {
-    override async onWillAppear(ev: WillAppearEvent<CounterSettings>): Promise<void> {
-        return ev.action.setTitle(`${ev.payload.settings.count ?? 0}`);
-    }
+    // override async onWillAppear(ev: WillAppearEvent<CounterSettings>): Promise<void> {
+    //     return ev.action.setTitle(`${ev.payload.settings.count ?? 0}`);
+    // }
 
     override async onKeyDown(ev: KeyDownEvent<CounterSettings>): Promise<void> {
         console.log("triggering macro...", ev);
-        // Update the count from the settings.
         const { settings } = ev.payload;
-        settings.fuckBy ??= 1;
-        settings.count = (settings.count ?? 0) + settings.fuckBy;
+        // settings.count = (settings.count ?? 0) + settings.increment
 
-        // this.actions.forEach((a: Action) => {
-        //     console.log("action", a);
-        // })
-        console.log("action", action);
-
-        const count = ev.payload.settings.count ?? 0;
-        const isRed = count % 2 === 0;
-        const svg = `<svg width="100" height="100">
-                                        <circle fill="${isRed ? "red" : "blue"}" r="45" cx="50" cy="50" ></circle>
-                                </svg>`;
-        // can set image path too
-        // await ev.action.setImage(`data:image/svg+xml,${encodeURIComponent(svg)}`, {
-        //     // target: Target.Software
-        //     // target: Target.Hardware
-        //     // target: Target.HardwareAndSoftware
-        // });
-
-        ev.action.setSettings({ count: count + 1 });
-
-        // await ev.action.showAlert();
-        // await ev.action.showOk();
-
-        // Update the current count in the action's settings, and change the title.
-        await ev.action.setSettings(settings);
-        await ev.action.setTitle(`${settings.count}`);
+        const uuid = settings.macro_uuid;
+        console.log('Macro UUID:', uuid);
+        await KeyboardMaestroHelper.executeMacro(uuid);
     }
 
     override async onSendToPlugin(ev: SendToPluginEvent<MySendToPlugin, CounterSettings>): Promise<void> | void {
@@ -107,5 +82,5 @@ type MySendToPlugin = {
  */
 type CounterSettings = {
     count?: number;
-    fuckBy?: number;
+    macro_uuid?: string;
 };
